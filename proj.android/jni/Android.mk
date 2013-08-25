c72498c3449e12126458f0e21f272e2f983557c0
zhenian <mail@zhenian.com>
2013-08-25 09:15:41 +0800

    code for android

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -8,7 +8,10 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+                   ../../Classes/HelloWorldScene.cpp \
+                   ../../Classes/CCProps.cpp \
+                   ../../Classes/IntroLayer.cpp \
+                   ../../Classes/CCFoePlane.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
